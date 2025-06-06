55f72bfd6d8db92d42cc018a9d65ea51eec3c8aa
nViNova <stephenjamesgonda@gmail.com>
2025-06-06 22:31:12 +0800

    feat: new asm file

--- input.asm
+++ input.asm
@@ -1,4 +1,17 @@
-rarb 12
-acc 1
-to-mba
-beqz 1
+# 10 x 10 grid
+# 5 registers, 4 bits each, 1 accumulator, 0 - 4
+
+# store player pos in reg 1 2
+
+# initialize vars
+rcrd 192
+acc 8
+to-mdc
+
+# increase column
+beqz-cf
+inc*-reg 4
+
+rot-rc
+to-mdc
+beqz 48
