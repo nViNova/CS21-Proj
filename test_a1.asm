9a3982921e9a68eb4668d0f308f626d893cc684c
Kourinx <160254275+Kourinx@users.noreply.github.com>
2025-06-06 23:47:42 +0800

    Add files via upload

---
+++ test_a1.asm
@@ -0,0 +1,54 @@
+rot-r
+rot-l
+rot-rc
+rot-lc
+from-mba
+to-mba
+from-mdc
+to-mdc
+addc-mba
+add-mba
+subc-mba
+sub-mba
+inc*-mba
+dec*-mba
+inc*-mdc
+dec*-mdc
+inc*-reg RC
+dec*-reg RC
+and-ba
+xor-ba
+or-ba
+and*-mba
+xor*-mba
+or*-mba
+to-reg RC
+from-reg RB
+clr-cf
+set-cf
+ret
+from-ioa
+inc
+bcd
+shutdown
+nop
+dec
+add 13
+sub 12
+and 1
+xor 2
+or 13
+r4 15
+rarb 1028
+rcrd 256
+acc 10
+b-bit 198
+bnz-a 1000
+bnz-b 99
+beqz 69
+bnez 420
+beqz-cf 69
+bnez-cf 420
+bnz-d 69
+b 420
+call 420
