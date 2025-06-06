db4fd7a0bd353ef35d1537260b8c65e2f8a3fc6e
nViNova <stephenjamesgonda@gmail.com>
2025-06-06 16:09:10 +0800

    feat: main method, testfile

--- A2.py
+++ A2.py
@@ -318,45 +318,9 @@ def emulate_instruction(instr: str):
     pc_update_int = (int(REG["PC"], 2) + 16) % 0b1111_1111_1111_11111
     REG["PC"] = f"{pc_update_int:016b}"
 
+def main_func(commands: list[str]):
 
-def test_overflowing_with_cf_addc_mba():
-    print("running tests for addc-mba")
-    REG["ACC"] = "1111"
-    MEM[REG["RB"] + REG["RA"]] = "1111"
-    REG["CF"] = "1"
-
-    emulate_instruction("addc-mba")
-
-    print(REG["CF"])
-    print(REG["ACC"])
-    assert REG["CF"] == "1"
-    assert REG["ACC"] == "1111"
-
-
-def test_overflowing_inc_mba():
-    print("running tests for inc-mba")
-
-    MEM[REG["RB"] + REG["RA"]] = "0111"
-    emulate_instruction("inc*-mba")
-    print(MEM[REG["RB"] + REG["RA"]])
-
-    MEM[REG["RB"] + REG["RA"]] = "1111"
-    emulate_instruction("inc*-mba")
-    print(MEM[REG["RB"] + REG["RA"]])
-
-
-def test_underflowing_dec_mba():
-    print("running tests for dec-mba")
-
-    MEM[REG["RB"] + REG["RA"]] = "0001"
-    emulate_instruction("dec*-mba")
-    print(MEM[REG["RB"] + REG["RA"]])
-
-    MEM[REG["RB"] + REG["RA"]] = "0000"
-    emulate_instruction("dec*-mba")
-    print(MEM[REG["RB"] + REG["RA"]])
-
-
-test_overflowing_with_cf_addc_mba()
-test_overflowing_inc_mba()
-test_underflowing_dec_mba()
+    while True:
+        curr_PC = int(REG["PC"], 2)
+        curr_PC //= 16
+        emulate_instruction(commands[curr_PC])
---
+++ test_A2.py
@@ -0,0 +1,43 @@
+from A2 import *
+
+def test_overflowing_with_cf_addc_mba():
+    print("running tests for addc-mba")
+    REG["ACC"] = "1111"
+    MEM[REG["RB"] + REG["RA"]] = "1111"
+    REG["CF"] = "1"
+
+    emulate_instruction("addc-mba")
+
+    print(REG["CF"])
+    print(REG["ACC"])
+    assert REG["CF"] == "1"
+    assert REG["ACC"] == "1111"
+
+
+def test_overflowing_inc_mba():
+    print("running tests for inc-mba")
+
+    MEM[REG["RB"] + REG["RA"]] = "0111"
+    emulate_instruction("inc*-mba")
+    print(MEM[REG["RB"] + REG["RA"]])
+
+    MEM[REG["RB"] + REG["RA"]] = "1111"
+    emulate_instruction("inc*-mba")
+    print(MEM[REG["RB"] + REG["RA"]])
+
+
+def test_underflowing_dec_mba():
+    print("running tests for dec-mba")
+
+    MEM[REG["RB"] + REG["RA"]] = "0001"
+    emulate_instruction("dec*-mba")
+    print(MEM[REG["RB"] + REG["RA"]])
+
+    MEM[REG["RB"] + REG["RA"]] = "0000"
+    emulate_instruction("dec*-mba")
+    print(MEM[REG["RB"] + REG["RA"]])
+
+
+test_overflowing_with_cf_addc_mba()
+test_overflowing_inc_mba()
+test_underflowing_dec_mba()
